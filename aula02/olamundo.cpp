41a5bb01c4f78f84010be0c704ebf79a2e95ed8e
Salustiano Oliveira <saluorodrigues@gmail.com>
2022-09-20 12:34:46 -0300

    aula02

---
+++ aula02/olamundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//Comentario em c++
+	//Comentario de linha
+	/*Comentario de 
+	bloco (multiplas linhas)*/
+using namespace std;
+//Funcao principal
+int main(){
+	//Programa que imprime mensagens para o usuario
+	printf("Ola mundo!\n");
+	printf("\nSalu, bem vindo a aula de Intro Prog.");
+	//O comando printf escreve na saida(prompt) 
+	//a mensagem entre aspas
+	system("pause");
+}
